727e00e835bbc3f33de961fecb13dab929046e20
Arjun Kasaudhan <33134982+arjungupta48@users.noreply.github.com>
2018-10-04 01:03:54 +0530

    Update distribution.component.spec.ts

--- src/app/distribution/distribution.component.spec.ts
+++ src/app/distribution/distribution.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DistributionComponent } from './distribution.component';
+import { DistributionComponent } from '../distribution/distribution.component';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { By} from '@angular/platform-browser';
 
 import { RouterModule } from '@angular/router';
 import { AppModule } from '../app.module';
@@ -12,7 +14,8 @@ describe('DistributionComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ RouterModule, AppModule ],
-      declarations: [ ]
+      declarations: [ ],
+      providers: [HttpClient, HttpHandler]
     })
     .compileComponents();
   }));
@@ -20,10 +23,41 @@ describe('DistributionComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DistributionComponent);
     component = fixture.componentInstance;
+    component.distributions = [
+      { "distributionId": 1, "fname": "Arjun", "lname": "Kasaudhan", "plan": "401K Plan", "ssn": "923-23-1177" },
+      { "distributionId": 2, "fname": "Gaurav", "lname": "Srivastava", "plan": "AL2", "ssn": "923-23-2277" }
+    ]
     fixture.detectChanges();
   });
 
   it('should create distribution component', () => {
     expect(component).toBeTruthy();
+  });
+  it('should call search function', () => {
+    spyOn(component, 'search').and.callThrough();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.debugElement.query(By.css('button')).triggerEventHandler('click', 1);
+      fixture.detectChanges();
+      expect(component.search).toHaveBeenCalled();
+    });
+  });
+  it('should search distributions with id equal to 1', () => {
+    spyOn(component, 'search').and.callThrough();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.debugElement.query(By.css('button')).triggerEventHandler('click', 1);
+      fixture.detectChanges();
+      expect(component.distributions[0].distributionId).toBe(1);
+    });
+  });
+  it('should search distributions and result should not be null', () => {
+    spyOn(component, 'search').and.callThrough();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.debugElement.query(By.css('button')).triggerEventHandler('click', 2);
+      fixture.detectChanges();
+      expect(component.distributions[0].distributionId).not.toEqual(null);
+    });
   });
 });
