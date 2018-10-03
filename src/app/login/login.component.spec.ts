a446d23d044c3aa7a5963724aaa50a4ad81479de
Arjun Kasaudhan <33134982+arjungupta48@users.noreply.github.com>
2018-10-04 00:53:31 +0530

    Update login.component.spec.ts

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,13 +2,17 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 
+import { RouterModule } from '@angular/router';
+import { AppModule } from '../app.module';
+
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [ RouterModule, AppModule ],
+      declarations: [ ]
     })
     .compileComponents();
   }));
@@ -19,7 +23,7 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create login component', () => {
     expect(component).toBeTruthy();
   });
 });
